f9b1608c004282d066d171b95002d181e9c7c2e5
Your Name <you@example.com>
2023-08-16 13:54:26 +0800

    678

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app.secret_key = 'nowcoder123'
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello,World!123234123"
+    return "789"
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
